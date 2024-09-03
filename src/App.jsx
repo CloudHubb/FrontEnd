9802e5b9ac3a7c7383f1762475e12b4fac6e536b
asdfghjk1052@naver.com <asdfghjk1052@naver.com>
2024-09-03 20:32:09 +0900

    [ :tada: | cloudhub 프론트 프로젝트 생성 ]

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+// library
+import { BrowserRouter } from "react-router-dom";
+
+// src/assets
+import "./assets/myreset.css";
+
+// src/routes
+import Router from "./routes";
+
+// src/components
+import Footer from "./components/footer/Footer";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Router />
+      <Footer />
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/routes/index.jsx
@@ -0,0 +1,46 @@
+// 예시
+import { Navigate, useRoutes } from "react-router-dom";
+import { routes } from "../constants/routes";
+import HomePage from "../pages/home/page";
+import CommunityPage from "../pages/community/page";
+import DataRoomPage from "../pages/data_room/page";
+import MyPage from "../pages/my_page/page";
+import SelfTestPage from "../pages/self_test/page";
+import Header from "../components/header/Header";
+
+export default function Router() {
+  const elements = [
+    {
+      path: "/",
+      element: <Header />,
+      children: [
+        {
+          path: routes.home,
+          element: <HomePage />,
+        },
+        {
+          path: routes.community,
+          element: <CommunityPage />,
+        },
+        {
+          path: routes.dataroom,
+          element: <DataRoomPage />,
+        },
+        {
+          path: routes.mypage,
+          element: <MyPage />,
+        },
+        {
+          path: routes.selftest,
+          element: <SelfTestPage />,
+        },
+        {
+          path: "/",
+          element: <Navigate replace to={routes.home} />,
+        },
+      ],
+    },
+  ];
+
+  return useRoutes(elements);
+}
