e72769dea149996388e18163b16a3d15d7457726
Youngbae1126 <rcplaza2663@gmail.com>
2024-09-22 02:50:40 +0900

    [ :art: | #5 | Style ] 자가진단 문항 카드 스타일 완성

---
+++ src/pages/self_test/_components/QuestionCard.jsx
@@ -0,0 +1,87 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+// Styles
+import '../_styles/QuestionCard.css';
+
+// Assets
+import ProgressBar from './ProgressBar';
+
+export default function QuestionCard({
+  question,
+  currentQuestion,
+  totalQuestions,
+  onAnswer,
+  onNext,
+  onPrevious,
+  answer,
+}) {
+  return (
+    <>
+      <div className="questionCardWrapper">
+        <ProgressBar
+          currentQuestion={currentQuestion}
+          totalQuestions={totalQuestions}
+        />
+        <section className="questionText">
+          {question.split('\n').map((line, index) => (
+            <React.Fragment key={index}>
+              <p>{line}</p>
+              <br />
+            </React.Fragment>
+          ))}
+        </section>
+
+        <section className="questionContent">
+          <button
+            onClick={() => onAnswer('예')}
+            id={`yesBtn-${answer === '예' ? 'set' : 'unset'}`}
+          >
+            예
+          </button>
+          <button
+            onClick={() => onAnswer('아니오')}
+            id={`noBtn-${answer === '아니오' ? 'set' : 'unset'}`}
+          >
+            아니오
+          </button>
+        </section>
+
+        <section className="prevNextWrapper">
+          {currentQuestion > 0 && (
+            <button onClick={onPrevious} id={'prevBtn'}>
+              이전
+            </button>
+          )}
+          {currentQuestion < totalQuestions - 1 ? (
+            <button onClick={onNext} disabled={answer === null} id={'nextBtn'}>
+              다음
+            </button>
+          ) : (
+            <button
+              onClick={() => alert('결과 보기')}
+              disabled={answer === null}
+              id={'showResultBtn'}
+            >
+              결과보기
+            </button>
+          )}
+        </section>
+      </div>
+    </>
+  );
+}
+
+QuestionCard.propTypes = {
+  question: PropTypes.string.isRequired,
+  currentQuestion: PropTypes.number.isRequired,
+  totalQuestions: PropTypes.number.isRequired,
+  onAnswer: PropTypes.func.isRequired,
+  onNext: PropTypes.func.isRequired,
+  onPrevious: PropTypes.func.isRequired,
+  answer: PropTypes.oneOfType([
+    PropTypes.bool,
+    PropTypes.string,
+    PropTypes.oneOf([null]),
+  ]),
+};
