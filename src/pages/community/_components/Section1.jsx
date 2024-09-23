8afa86a727cf3254b850a512cb086646ac50f797
Youngbae1126 <rcplaza2663@gmail.com>
2024-09-23 18:47:20 +0900

    [ :art: | #6 | Style ] 커뮤니티 메인 페이지 퍼블리싱 완료

--- src/pages/community/_components/Section1.jsx
+++ src/pages/community/_components/Section1.jsx
@@ -17,7 +17,6 @@ export default function Section1() {
         <div className="todayPopularList">
           <span>오늘의 인기글</span>
           <div className="todayPopularListTitleBox" />
-
           <Swiper
             slidesPerView={2.5}
             spaceBetween={10}
@@ -38,6 +37,7 @@ export default function Section1() {
               <TodayPopularCard />
             </SwiperSlide>
           </Swiper>
+          <div className="todayPopularListTitleBox2" />
         </div>
       </div>
     </>
--- src/pages/community/_components/Section2.jsx
+++ src/pages/community/_components/Section2.jsx
@@ -1,5 +1,24 @@
 import '../_styles/Section2.css';
 
+import BoardCard from '../../../components/communityCard/boardCard';
+
 export default function Section2() {
-  return <></>;
+  return (
+    <>
+      <div className="section2Wrapper">
+        <div className="boardFilterWrapper">
+          <span>최신순</span>
+          <span>조회순</span>
+        </div>
+        <div className="boardList">
+          <BoardCard />
+          <BoardCard />
+          <BoardCard />
+          <BoardCard />
+          <BoardCard />
+          <BoardCard />
+        </div>
+      </div>
+    </>
+  );
 }
