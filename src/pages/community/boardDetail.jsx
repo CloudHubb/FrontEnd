e949c69362ab272e809f600028b75b7bc814e74f
Youngbae1126 <rcplaza2663@gmail.com>
2024-09-24 21:22:15 +0900

    [ :art: | #6 | Style ] 게시글 상세보기 타이틀 퍼블리싱 완료

--- src/pages/community/boardDetail.jsx
+++ src/pages/community/boardDetail.jsx
@@ -1,9 +1,19 @@
+// Styles
 import './_styles/boardDetail.css';
+
+// Assets
+import TitleWrapper from './_components/titleToggle';
+import BoardTitle from './_components/boardTitle';
+import BoardInfo from './_components/boardInfo';
 
 export default function boardDetail() {
   return (
     <>
-      <h2>게시글 조회 페이지</h2>
+      <div className="boardDetailWrapper">
+        <TitleWrapper />
+        <BoardTitle type={'detail'} />
+        <BoardInfo type={'detail'} />
+      </div>
     </>
   );
 }
