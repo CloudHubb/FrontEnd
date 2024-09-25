8ba108ef3727d77f0a3d229d46d6bb900d9c02bf
Youngbae1126 <rcplaza2663@gmail.com>
2024-09-25 21:06:24 +0900

    [ :bug: | #6 | Fix ] 빌드 에러 해결을 위해 vite.config.js 파일 수정

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    svgr({
-      svgrOptions: {
-        icon: true, // SVG를 React 컴포넌트로 처리
-      },
-    }),
-  ],
+  plugins: [react()],
 });
